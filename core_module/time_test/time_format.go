e86ba020b10efe45e99c9dc50f7a95dcc7cfc592
walkskyer <walkskyer@qq.com>
2022-08-29 18:30:32 +0800

    时间格式转换示例

--- core_module/time_test/time_format.go
+++ core_module/time_test/time_format.go
@@ -12,4 +12,19 @@ func main() {
 		fmt.Println(err)
 	}
 	fmt.Println(x)
+
+	s = ""
+	x, err = time.Parse("2006-1-2 15:04:05", s)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(x)
+
+	b := []byte("\"\"")
+	x, err = time.Parse("2006-1-2", string(b[1:len(b)-1]))
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(x)
+
 }
