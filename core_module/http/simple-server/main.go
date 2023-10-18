43bd71a2b965a5363b6a0a488e5e107300e2a37a
walkskyer <walkskyer@qq.com>
2023-10-18 17:00:31 +0800

    feat(http/simple-server):http-server show some request info

--- core_module/http/simple-server/main.go
+++ core_module/http/simple-server/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -12,6 +14,20 @@ func main() {
 	})
 	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "Hello world!")
+	})
+	http.HandleFunc("/show-request", func(w http.ResponseWriter, r *http.Request) {
+		data := map[string]any{
+			"HEADER": r.Header,
+			"URL":    r.URL,
+		}
+		jsonData, err := json.Marshal(data)
+		if err != nil {
+			fmt.Fprintln(w, err.Error())
+			return
+		}
+		var jsonStr bytes.Buffer
+		json.Indent(&jsonStr, jsonData, "", "  ")
+		fmt.Fprintln(w, jsonStr.String())
 	})
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
