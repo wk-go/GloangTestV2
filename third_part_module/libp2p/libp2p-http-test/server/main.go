e64a96626d4985dfb474bfba622451add7b1dd65
walkskyer <walkskyer@qq.com>
2023-10-18 16:36:36 +0800

    feat(libp2p/ping-test):libp2p-http-test 增加密钥保存和恢复功能

--- third_part_module/libp2p/libp2p-http-test/server/main.go
+++ third_part_module/libp2p/libp2p-http-test/server/main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"encoding/hex"
 	"fmt"
 	"github.com/libp2p/go-libp2p"
 	gostream "github.com/libp2p/go-libp2p-gostream"
 	p2phttp "github.com/libp2p/go-libp2p-http"
+	"github.com/libp2p/go-libp2p/core/crypto"
 	"github.com/libp2p/go-libp2p/core/host"
 	"github.com/libp2p/go-libp2p/core/peer"
 	"github.com/libp2p/go-libp2p/core/peerstore"
 	"github.com/libp2p/go-libp2p/core/protocol"
+	"io"
 	"net/http"
 	"os"
 	"os/signal"
@@ -20,8 +23,69 @@ var (
 )
 
 func main() {
+	configDir := "./config"
+	port := "0"
+
+	var cryptoFileDir string
+	if len(os.Args) > 1 {
+		cryptoFileDir = configDir + "/client"
+	} else {
+		cryptoFileDir = configDir + "/server"
+		port = "51080"
+	}
+
+	if _, err := os.Stat(cryptoFileDir); os.IsNotExist(err) {
+		if err = os.MkdirAll(cryptoFileDir, 0755); err != nil {
+			panic(err)
+		}
+	}
+	var privKey crypto.PrivKey
+	keyFile := cryptoFileDir + "/private.key"
+	if _, err := os.Stat(keyFile); os.IsNotExist(err) {
+		privKey, _, err = crypto.GenerateKeyPair(crypto.RSA, 2048)
+		if err != nil {
+			panic(err)
+		}
+		func() {
+			f, err := os.OpenFile(keyFile, os.O_CREATE|os.O_WRONLY, 0755)
+			if err != nil {
+				panic(err)
+			}
+			defer f.Close()
+
+			keyData, err := privKey.Raw()
+			if err != nil {
+				panic(err)
+			}
+
+			if _, err := hex.NewEncoder(f).Write(keyData); err != nil {
+				panic(err)
+			}
+		}()
+	} else {
+		func() {
+			f, err := os.Open(keyFile)
+			if err != nil {
+				panic(err)
+			}
+			defer f.Close()
+			b, err := io.ReadAll(f)
+			if err != nil {
+				panic(err)
+			}
+			var keyResult = make([]byte, len(b))
+			n, err := hex.Decode(keyResult, b)
+			privKey, err = crypto.UnmarshalRsaPrivateKey(keyResult[:n])
+			if err != nil {
+				panic(err)
+			}
+		}()
+
+	}
+
 	host, err := libp2p.New(
-		libp2p.ListenAddrStrings("/ip4/127.0.0.1/tcp/0"),
+		libp2p.ListenAddrStrings("/ip4/127.0.0.1/tcp/"+port),
+		libp2p.Identity(privKey),
 	)
 	if err != nil {
 		panic(err)
@@ -78,7 +142,8 @@ func httpClient(host host.Host, addr string) {
 	tr.RegisterProtocol("libp2p", p2phttp.NewTransport(host, p2phttp.ProtocolOption(myProtocol)))
 	client := &http.Client{Transport: tr}
 
-	res, err := client.Get(fmt.Sprintf("libp2p://%s/hello", addrInfo.ID.String()))
+	requestUrl := fmt.Sprintf("libp2p://%s/hello", addrInfo.ID.String())
+	res, err := client.Get(requestUrl)
 	if err != nil {
 		panic(err)
 	}
