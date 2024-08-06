89f04e321ef66e9a6105c4bac4d7b81c1f71994e
walkskyer <walkskyer@qq.com>
2024-08-06 10:30:56 +0800

    feat(basic/struct):测试结构体多重继承 方法和属性调用

--- basic/operators/op_test.go
+++ basic/operators/op_test.go
@@ -1,6 +1,9 @@
 package operators
 
-import "testing"
+import (
+	"strconv"
+	"testing"
+)
 
 func _bool(b bool, t *testing.T) bool {
 	t.Log("bool:", b)
@@ -68,4 +71,50 @@ func TestTheANDOperater(t *testing.T) {
 	t.Log("--------------------------")
 
 	t.Log("test \"&&\" && operator end!!")
+}
+
+type A struct {
+	a int
+	b int
+}
+
+func (a A) String() string {
+	return "[A] a:" + strconv.Itoa(a.a) + " b:" + strconv.Itoa(a.b)
+}
+
+type B struct {
+	a int
+	b int
+}
+
+func (b B) String() string {
+	return "[B] a:" + strconv.Itoa(b.a) + " b:" + strconv.Itoa(b.b)
+}
+
+// C 继承A和B 更像A和B
+type C struct {
+	A
+	B
+}
+
+// D 继承A和B
+type D struct {
+	A
+	B
+}
+
+// D 拥有自己的小性格，覆盖A和B的String方法
+func (d D) String() string {
+	return "[D] a:" + strconv.Itoa(d.A.a) + " b:" + strconv.Itoa(d.B.b)
+}
+
+// 测试结构体多重继承 访问方法和属性的效果
+func TestStructMultipleInheritance(t *testing.T) {
+	c := C{A: A{a: 1, b: 2}, B: B{a: 3, b: 4}}
+	t.Log(c)            // 分别调用A和B的String方法
+	t.Log(c.A.String()) // 调用A的String方法
+
+	d := D{A: A{a: 11, b: 12}, B: B{a: 13, b: 14}}
+	t.Log(d)          // 调用D的String方法
+	t.Log(d.String()) // 调用D的String方法
 }
